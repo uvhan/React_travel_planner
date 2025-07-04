412ab0bf4d8fdd23425b591936ede3f9d388bb38
uvhan <uvhan.sg@gmail.com>
2025-07-04 15:52:52 +0530

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://uvhan.github.io/React_travel_planner/'
+  base: '/React_travel_planner/'
 })
