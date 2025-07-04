4f5f30925ee12c921fc338e7fd378dd8ba3d8817
uvhan <uvhan.sg@gmail.com>
2025-07-04 16:01:39 +0530

    router bug fix

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -6,16 +6,14 @@ import Service from "./routes/Service.jsx"
 import { Route, Routes } from "react-router-dom"
 function App() {
   return (
-    <>
-      <div>
+
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
           <Route path="/service" element={<Service/>}/>
           <Route path="/contact" element={<Contact/>}/>
         </Routes>
-      </div>
-    </>
+
   )
 }
 export default App
